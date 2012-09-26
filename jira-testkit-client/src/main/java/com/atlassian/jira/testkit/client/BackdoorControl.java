93a948c0f2c300c805e4bedd4e6b11a9f367706b
Luis Miranda <lmiranda@atlassian.com>
2012-09-26 12:13:21 +1000

    deprecated most get/post/put/delete methods in BackdoorControl. JRADEV-14344
    I've backported the JsonMediaTypeFilter and BackdoorLoggingFilter to the
    5.0 TestKit, so these methods are no longer useful. deprecating these
    methods to smoothe the transition from TestKit 5.0-xx to 5.2-xx, where
    these methods no longer exist.

--- jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/BackdoorControl.java
+++ jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/BackdoorControl.java
@@ -4,28 +4,43 @@ import com.atlassian.jira.testkit.client.dump.FuncTestTimer;
 import com.atlassian.jira.testkit.client.dump.TestInformationKit;
 import com.atlassian.jira.testkit.client.log.FuncTestLogger;
 import com.atlassian.jira.testkit.client.log.FuncTestLoggerImpl;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.ClientFilter;
 import org.apache.commons.lang.StringUtils;
 
 import javax.ws.rs.core.MediaType;
 
 /**
- * Parent class for Backdoor controllers making func-test REST requests to
- * set up test state without the UI overhead.
- *
- * See the Backdoor classes in com.atlassian.jira.pageobjects.config of the
- * jira-func-test-plugin module for more.
+ * Parent class for Backdoor controllers making func-test REST requests to set up test state without the UI overhead.
  *
  * @since v5.0
  */
-public abstract class BackdoorControl<T extends BackdoorControl<T>> extends RestApiClient<T> implements FuncTestLogger
+public abstract class BackdoorControl<T extends BackdoorControl<T>> extends RestApiClient<T>
 {
-    private static final String BACKDOOR = "Backdoor Shenanigans";
+    /**
+     * The default REST path for the TestKit resources.
+     */
+    public static final String DEFAULT_REST_PATH = "testkit-test";
 
-    protected String rootPath;
-    private FuncTestLoggerImpl logger;
+    /**
+     * The JIRA base URL.
+     */
+    protected final String rootPath;
 
+    /**
+     * The FuncTestLogger to use for logging.
+     */
+    protected final FuncTestLogger logger;
+
+    /**
+     * Creates a new BackdoorControl.
+     *
+     * @param environmentData a JIRAEnvironmentData
+     */
     public BackdoorControl(JIRAEnvironmentData environmentData)
     {
         super(environmentData);
@@ -34,102 +49,159 @@ public abstract class BackdoorControl<T extends BackdoorControl<T>> extends Rest
     }
 
     /**
-     * Helper method for making easy GET calls. Feel free to add overloads to accept paths and parameter maps.
+     * Convenience method that simply calls {@code WebResource.get(String.class)} on the passed-in WebResource.
+     * <p/>
+     * <em>This method is often used for mutative operations</em> which should really be using a POST instead, but doing
+     * a GET is much more convenient since you can do it right from your browser when testing a backdoor and this is
+     * only testing code so it's OK.
      *
-     * @param webResource web resource
-     * @return response string
+     * @param webResource the WebResource to perform the GET on
      */
-    protected String get(final WebResource webResource)
+    protected final String get(final WebResource webResource)
     {
-        final FuncTestTimer timer = TestInformationKit.pullTimer(BACKDOOR);
-
-        String result = webResource.get(String.class);
-
-        logTime(webResource, "GET", timer.end());
-        return result;
+        return webResource.get(String.class);
     }
 
+    /**
+     * Deprecated: use {@link WebResource#get(com.sun.jersey.api.client.GenericType)} instead.
+     * <p/>
+     * <b>This method will be removed in 5.2.</b>
+     */
+    @Deprecated
     protected <T> T get(WebResource webResource, GenericType<T> returnType)
     {
-        final FuncTestTimer timer = TestInformationKit.pullTimer(BACKDOOR);
-
-        T result = webResource.type(MediaType.APPLICATION_JSON_TYPE).get(returnType);
-
-        logTime(webResource, "GET", timer.end());
-        return result;
+        return webResource.get(returnType);
     }
 
+    /**
+     * Deprecated: use {@link WebResource#get(Class)} instead.
+     * <p/>
+     * <b>This method will be removed in 5.2.</b>
+     */
+    @Deprecated
     protected <T> T get(WebResource webResource, Class<T> returnClass)
     {
-        final FuncTestTimer timer = TestInformationKit.pullTimer(BACKDOOR);
-
-        T result = webResource.type(MediaType.APPLICATION_JSON_TYPE).get(returnClass);
-
-        logTime(webResource, "GET", timer.end());
-        return result;
+        return webResource.get(returnClass);
     }
 
+    /**
+     * Deprecated: use {@link WebResource#get(Class)} instead.
+     * <p/>
+     * <b>This method will be removed in 5.2.</b>
+     */
+    @Deprecated
     protected long getId(final WebResource webResource)
     {
         return Long.parseLong(get(webResource));
     }
 
+    /**
+     * Deprecated: use {@link com.sun.jersey.api.client.WebResource#post()} instead.
+     * <p/>
+     * <b>This method will be removed in 5.2.</b>
+     */
+    @Deprecated
     protected void post(WebResource webResource)
     {
-        final FuncTestTimer timer = TestInformationKit.pullTimer(BACKDOOR);
-
         webResource.post();
-
-        logTime(webResource, "POST", timer.end());
-    }
-
-    protected <T> T post(WebResource webResource, Object bean, Class<T> returnClass)
-    {
-        final FuncTestTimer timer = TestInformationKit.pullTimer(BACKDOOR);
-        T result = webResource.type(MediaType.APPLICATION_JSON_TYPE).post(returnClass, bean);
-        logTime(webResource, "POST", timer.end());
-        return result;
-    }
-
-    protected void post(WebResource webResource, Object bean)
-    {
-        final FuncTestTimer timer = TestInformationKit.pullTimer(BACKDOOR);
-        webResource.type(MediaType.APPLICATION_JSON_TYPE).post(bean);
-        logTime(webResource, "POST", timer.end());
-    }
-
-    protected void delete(WebResource webResource)
-    {
-        final FuncTestTimer timer = TestInformationKit.pullTimer(BACKDOOR);
-        webResource.type(MediaType.APPLICATION_JSON_TYPE).delete();
-        logTime(webResource, "DELETE", timer.end());
     }
 
     /**
-     * Creates the resource that corresponds to the root of the func-test REST API.
+     * Deprecated: use {@link WebResource#post(Class, Object)} instead.
+     * <p/>
+     * <b>This method will be removed in 5.2.</b>
+     */
+    protected <T> T post(WebResource webResource, Object bean, Class<T> returnClass)
+    {
+        return webResource.type(MediaType.APPLICATION_JSON_TYPE).post(returnClass, bean);
+    }
+
+    /**
+     * Deprecated: use {@link WebResource#post(Object)} instead.
+     * <p/>
+     * <b>This method will be removed in 5.2.</b>
+     */
+    protected void post(WebResource webResource, Object bean)
+    {
+        webResource.type(MediaType.APPLICATION_JSON_TYPE).post(bean);
+    }
+
+    /**
+     * Deprecated: use {@link WebResource#delete()} instead.
+     * <p/>
+     * <b>This method will be removed in 5.2.</b>
+     */
+    protected void delete(WebResource webResource)
+    {
+        webResource.type(MediaType.APPLICATION_JSON_TYPE).delete();
+    }
+
+    /**
+     * Creates the resource that corresponds to the root of the TestKit REST API. Note that the created {@code
+     * WebResource} has the following properties: <ul> <li>it logs all GET/POST/etc requests made through it</li> <li>it
+     * sets the <code>Content-Type: {@value MediaType#APPLICATION_JSON}</code> by default (override with {@link
+     * WebResource#type(javax.ws.rs.core.MediaType)})</li> </ul>
      *
-     * @return a WebResource for the REST API root
+     * @return a WebResource for the TestKit REST API root
      */
     protected WebResource createResource()
     {
-        return resourceRoot(rootPath).path("rest").path("testkit-test").path("1.0");
+        WebResource resource = resourceRoot(rootPath).path("rest").path(getRestModulePath()).path("1.0");
+        resource.addFilter(new BackdoorLoggingFilter());
+        resource.addFilter(new JsonMediaTypeFilter());
+
+        return resource;
     }
 
-    @Override
-    public void log(Object logData)
+    /**
+     * Returns the REST path used in this plugin's {@code atlassian-plugin.xml} (e.g. {@code &lt;rest path="..."&gt;}).
+     * The default value is "{@value #DEFAULT_REST_PATH}".
+     *
+     * @return the REST path used in this plugin's {@code atlassian-plugin.xml} (e.g. {@code &lt;rest path="..."&gt;}).
+     */
+    protected String getRestModulePath()
     {
-        logger.log(logData);
+        return DEFAULT_REST_PATH;
     }
 
-    @Override
-    public void log(Throwable t)
+    /**
+     * Logs all Backdoor requests using the FuncTestLogger.
+     *
+     * @see FuncTestLogger#log(Object)
+     */
+    protected class BackdoorLoggingFilter extends ClientFilter
     {
-        logger.log(t);
+        @Override
+        public ClientResponse handle(ClientRequest request) throws ClientHandlerException
+        {
+            FuncTestTimer timer = TestInformationKit.pullTimer("Backdoor Shenanigans");
+            ClientResponse response = getNext().handle(request);
+            logRequest(request, timer.end());
+
+            return response;
+        }
+
+        private void logRequest(ClientRequest request, long howLong)
+        {
+            String relativePath = StringUtils.removeStart(request.getURI().getPath(), createResource().getURI().getPath());
+            logger.log(String.format("Backdoor %-6s in %5dms  %s", request.getMethod(), howLong, relativePath));
+        }
     }
 
-    private void logTime(WebResource webResource, String type, long howLong)
+    /**
+     * Sets the {@code Content-Type} header to "{@value MediaType#APPLICATION_JSON}" if not already set.
+     */
+    protected static class JsonMediaTypeFilter extends ClientFilter
     {
-        String relativePath = StringUtils.removeStart(webResource.getURI().getPath(), createResource().getURI().getPath().toString());
-        log(String.format("Backdoor %4s in %5dms  %s", type, howLong, relativePath));
+        @Override
+        public ClientResponse handle(ClientRequest request) throws ClientHandlerException
+        {
+            if (request.getEntity() != null && !request.getHeaders().containsKey("Content-Type"))
+            {
+                request.getHeaders().putSingle("Content-Type", MediaType.APPLICATION_JSON);
+            }
+
+            return getNext().handle(request);
+        }
     }
 }
