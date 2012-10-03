3d42eef3c40b32811670a5ca1db46eea84923760
Kenny MacLeod <kmacleod@atlassian.com>
2012-10-03 13:18:41 +1000

    Refactor TestKitLocalEnvironmentData constructor to improve clarity JRADEV-15308

--- jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/util/TestKitLocalEnvironmentData.java
+++ jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/util/TestKitLocalEnvironmentData.java
@@ -23,20 +23,23 @@ public class TestKitLocalEnvironmentData extends AbstractEnvironmentData
     private static final String DEFAULT_EDITION = "standard";
     private static final String DEFAULT_XML_DATA_LOCATION = "./src/test/xml";
     private static final String DEFAULT_PROPERTIES_FILENAME = "localtest.properties";
+    private static final String DEFAULT_CREATE_DUMMY_TENANT = "false";
+
     private static final String JIRA_PROTOCOL = "jira.protocol";
     private static final String JIRA_HOST = "jira.host";
     private static final String JIRA_PORT = "jira.port";
     private static final String JIRA_XML_DATA_LOCATION = "jira.xml.data.location";
     private static final String JIRA_CONTEXT = "jira.context";
     private static final String JIRA_TENANT = "jira.tenant";
+    private static final String JIRA_EDITION = "jira.edition";
     private static final String CREATE_DUMMY_TENANT = "jira.create.dummy.tenant";
     private static final String TEST_SERVER_PROPERTIES = "test.server.properties";
 
-    private final String context;
+    private final String contextPath;
     private final String tenant;
     private final boolean shouldCreateDummyTenant;
-    private final URL url;
-    private File xmlDataLocation;
+    private final URL baseUrl;
+    private final File xmlDataLocation;
     private final String edition;
    
     public TestKitLocalEnvironmentData()
@@ -49,45 +52,51 @@ public class TestKitLocalEnvironmentData extends AbstractEnvironmentData
         this(loadProperties(TEST_SERVER_PROPERTIES, DEFAULT_PROPERTIES_FILENAME), xmlDataLocation);
     }
 
-    public TestKitLocalEnvironmentData(Properties properties, @Nullable String xmlDataLocation)
+    public TestKitLocalEnvironmentData(Properties properties, @Nullable String xmlDataLocationOverride)
     {
         super(properties);
+        this.contextPath = getEnvironmentProperty(JIRA_CONTEXT, DEFAULT_CONTEXT);
+
+        final String xmlDataLocationProperty = getEnvironmentProperty(JIRA_XML_DATA_LOCATION, DEFAULT_XML_DATA_LOCATION).trim();
+        this.xmlDataLocation = resolveXmlDataLocation(xmlDataLocationOverride, xmlDataLocationProperty);
+
         final String protocol = getEnvironmentProperty(JIRA_PROTOCOL, DEFAULT_PROTOCOL);
         final String host = getEnvironmentProperty(JIRA_HOST, DEFAULT_HOST);
         final String port = getEnvironmentProperty(JIRA_PORT, DEFAULT_PORT);
-        final String propertyString = getEnvironmentProperty(JIRA_XML_DATA_LOCATION, DEFAULT_XML_DATA_LOCATION).trim();
-        final String contextPath = getEnvironmentProperty(JIRA_CONTEXT, DEFAULT_CONTEXT);
+        this.baseUrl = constructBaseUrl(protocol, host, port, contextPath);
 
-        File unresolvedFileLocation = new File(xmlDataLocation != null ? xmlDataLocation : propertyString);
-
-        try
-        {
-            this.xmlDataLocation = unresolvedFileLocation.getCanonicalFile();
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException("IOException trying to resolve file " + unresolvedFileLocation);
-        }
-
-        if (!this.xmlDataLocation.exists())
-        {
-			throw new RuntimeException(String.format("Cannot find xml data location: '%s'", unresolvedFileLocation));
-        }
-
-        String baseUrl = protocol + "://" + host + ":" + port + contextPath;
-
-        this.context = contextPath;
         this.tenant = getEnvironmentProperty(JIRA_TENANT, null);
-        this.shouldCreateDummyTenant = Boolean.parseBoolean(getEnvironmentProperty(CREATE_DUMMY_TENANT, "false"));
+        this.shouldCreateDummyTenant = Boolean.parseBoolean(getEnvironmentProperty(CREATE_DUMMY_TENANT, DEFAULT_CREATE_DUMMY_TENANT));
+        this.edition = getEnvironmentProperty(JIRA_EDITION, DEFAULT_EDITION);
+    }
+
+    private static URL constructBaseUrl(String protocol, String host, String port, String contextPath) {
+        String baseUrl = protocol + "://" + host + ":" + port + contextPath;
         try
         {
-            this.url = new URL(baseUrl);
+            return new URL(baseUrl);
         }
         catch (MalformedURLException e)
         {
             throw new RuntimeException("Malformed URL " + baseUrl);
         }
-        this.edition = getEnvironmentProperty("jira.edition", DEFAULT_EDITION);
+    }
+
+    private static File resolveXmlDataLocation(String xmlDataLocationOverride, String xmlDataLocationProperty) {
+        File unresolvedLocation = new File(xmlDataLocationOverride != null ? xmlDataLocationOverride : xmlDataLocationProperty);
+        try
+        {
+            File canonicalLocation = unresolvedLocation.getCanonicalFile();
+            if (!canonicalLocation.exists())
+            {
+                throw new RuntimeException(String.format("Cannot find xml data location: '%s'", unresolvedLocation));
+            }
+            return canonicalLocation;
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("IOException trying to resolve file " + unresolvedLocation);
+        }
     }
 
     public static Properties loadProperties(String key, String def)
@@ -125,7 +134,7 @@ public class TestKitLocalEnvironmentData extends AbstractEnvironmentData
 
     public String getContext()
     {
-        return context;
+        return contextPath;
     }
 
     public String getTenant()
@@ -140,7 +149,7 @@ public class TestKitLocalEnvironmentData extends AbstractEnvironmentData
 
     public URL getBaseUrl()
     {
-        return url;
+        return baseUrl;
     }
 
     public File getXMLDataLocation()
