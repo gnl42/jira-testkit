683ab2e8100a8c31a7d7b5c2465b479222a43c08
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2013-03-13 14:17:41 +0100

    NONE: updated copyright notice header to each file

--- jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/restclient/FilterClient.java
+++ jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/restclient/FilterClient.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright © 2012 - 2013 Atlassian Corporation Pty Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package com.atlassian.jira.testkit.client.restclient;
 
 import com.atlassian.jira.testkit.client.JIRAEnvironmentData;
@@ -12,8 +21,6 @@ import javax.ws.rs.core.MediaType;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
-
-import static com.atlassian.jira.rest.api.util.StringList.fromList;
 
 /**
  * Client for the Filter resource.
