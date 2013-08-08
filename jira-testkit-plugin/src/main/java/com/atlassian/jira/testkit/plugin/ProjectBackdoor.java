bc984027bc66264c3e7691dfbab7134ce2835c3c
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2013-08-08 10:34:38 +0200

    JRADEV-22253: updated project service, need to release testkit, also moved testkit to use JIRA 6.1 and JIRA REST separately

--- jira-testkit-plugin/src/main/java/com/atlassian/jira/testkit/plugin/ProjectBackdoor.java
+++ jira-testkit-plugin/src/main/java/com/atlassian/jira/testkit/plugin/ProjectBackdoor.java
@@ -21,6 +21,7 @@ import com.atlassian.jira.project.Project;
 import com.atlassian.jira.project.ProjectAssigneeTypes;
 import com.atlassian.jira.scheme.Scheme;
 import com.atlassian.jira.testkit.plugin.util.CacheControl;
+import com.atlassian.jira.user.ApplicationUsers;
 import com.atlassian.jira.user.util.UserUtil;
 import com.atlassian.jira.util.ErrorCollection;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
@@ -82,7 +83,7 @@ public class ProjectBackdoor
         ErrorCollection errorCollection = new EmptyErrorCollection();
         ProjectService.CreateProjectValidationResult result = new ProjectService.CreateProjectValidationResult(
                 errorCollection, name, key, "This project is awesome", lead, null, AssigneeTypes.PROJECT_LEAD,
-                null);
+                null, ApplicationUsers.from(getUserWithAdminPermission()));
         Project project = projectService.createProject(result);
 
         // Add the schemes
