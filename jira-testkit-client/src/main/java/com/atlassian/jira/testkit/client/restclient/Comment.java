a6e292add4174c74859dcd71c134569ca2610dcd
Filip Rogaczewski <frogaczewski@atlassian.com>
2014-03-11 14:44:30 +1100

    DESK-2031: Set comment properties during in comment resource

--- jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/restclient/Comment.java
+++ jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/restclient/Comment.java
@@ -9,6 +9,16 @@
 
 package com.atlassian.jira.testkit.client.restclient;
 
+import java.util.List;
+import java.util.Map;
+
+import com.atlassian.jira.util.json.JSONObject;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 /**
@@ -17,7 +27,8 @@ import org.codehaus.jackson.map.annotate.JsonSerialize;
  * @since v4.3
  */
 @JsonSerialize
-public class Comment
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class    Comment
 {
     public String self;
     public String id;
@@ -28,6 +39,7 @@ public class Comment
     public UserJson author;
     public UserJson updateAuthor;
     public Visibility visibility;
+    public List<CommentProperty> properties;
 
     public Comment()
     {
@@ -37,5 +49,48 @@ public class Comment
     {
         this.body = body;
         this.visibility = new Visibility("ROLE", roleLevel);
+    }
+
+    public void setProperties(List<Map<String, Object>> properties)
+    {
+        this.properties = Lists.transform(properties, new Function<Map<String, Object>, CommentProperty>()
+        {
+            @Override
+            public CommentProperty apply(final Map<String, Object> commentProperty)
+            {
+                String key = (String) commentProperty.get("key");
+                JSONObject value = new JSONObject((Map<String, Object>) commentProperty.get("value"));
+                return new CommentProperty(key, value.toString());
+            }
+        });
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class CommentProperty
+    {
+        @JsonProperty
+        public String key;
+        @JsonProperty
+        public String value;
+
+        public CommentProperty()
+        {
+        }
+
+        public CommentProperty(String key, String value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey()
+        {
+            return key;
+        }
+
+        public String getValue()
+        {
+            return value;
+        }
     }
 }
