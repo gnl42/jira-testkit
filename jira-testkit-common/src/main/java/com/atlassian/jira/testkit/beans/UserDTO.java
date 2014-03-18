748f5bf54d1904d0a92907c531c9531172a9773e
Andrew Swan <aswan@atlassian.com>
2014-03-18 13:17:40 +1100

    NONE Added UserDTO to common module

--- jira-testkit-common/src/main/java/com/atlassian/jira/testkit/beans/UserDTO.java
+++ jira-testkit-common/src/main/java/com/atlassian/jira/testkit/beans/UserDTO.java
@@ -1,12 +1,17 @@
 package com.atlassian.jira.testkit.beans;
 
+import com.atlassian.crowd.embedded.api.User;
 import com.atlassian.jira.user.ApplicationUser;
+import org.apache.commons.lang3.Validate;
+import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
+import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
+
 /**
- * A Data Transfer Object for serialization of an ApplicationUser into JSON.
+ * A Data Transfer Object for serialization of an ApplicationUser to and from JSON.
  */
-@SuppressWarnings("all")
 public class UserDTO
 {
     @JsonProperty
@@ -30,14 +35,149 @@ public class UserDTO
     @JsonProperty
     private final String username;
 
+    @JsonCreator
+    public UserDTO(
+            @JsonProperty("active") final boolean active,
+            @JsonProperty("directoryId") final long directoryId,
+            @JsonProperty("displayName") final String displayName,
+            @JsonProperty("email") final String email,
+            @JsonProperty("key") final String key,
+            @JsonProperty("name") final String name,
+            @JsonProperty("username") final String username)
+    {
+        Validate.notBlank(key);
+        Validate.notBlank(username);
+        this.active = active;
+        this.directoryId = directoryId;
+        this.displayName = displayName;
+        this.email = email;
+        this.key = key;
+        this.name = name;
+        this.username = username;
+    }
+
     public UserDTO(final ApplicationUser user)
     {
-        this.active = user.isActive();
-        this.directoryId = user.getDirectoryId();
-        this.displayName = user.getDisplayName();
-        this.email = user.getEmailAddress();
-        this.key = user.getKey();
-        this.name = user.getName();
-        this.username = user.getUsername();
+        this(
+                user.isActive(),
+                user.getDirectoryId(),
+                user.getDisplayName(),
+                user.getEmailAddress(),
+                user.getKey(),
+                user.getName(),
+                user.getUsername()
+        );
+    }
+
+    public String getKey()
+    {
+        return key;
+    }
+
+    // For debugging only
+    @Override
+    public String toString()
+    {
+        return reflectionToString(this, SHORT_PREFIX_STYLE);
+    }
+
+    /**
+     * Returns an ApplicationUser whose values reflect those of this DTO.
+     *
+     * @param directoryUser the directory user to which to delegate as
+     * necessary (required)
+     * @return a non-null result
+     */
+    public ApplicationUser asApplicationUser(final User directoryUser)
+    {
+        Validate.notNull(directoryUser);
+        return new ApplicationUser()
+        {
+            @Override
+            public String getKey()
+            {
+                return key;
+            }
+
+            @Override
+            public String getUsername()
+            {
+                return username;
+            }
+
+            @Override
+            public String getName()
+            {
+                return name;
+            }
+
+            @Override
+            public long getDirectoryId()
+            {
+                return directoryId;
+            }
+
+            @Override
+            public boolean isActive()
+            {
+                return active;
+            }
+
+            @Override
+            public String getEmailAddress()
+            {
+                return email;
+            }
+
+            @Override
+            public String getDisplayName()
+            {
+                return displayName;
+            }
+
+            @Override
+            public User getDirectoryUser()
+            {
+                // Sadly, ApplicationUser and User share all these properties
+                return new User() {
+
+                    @Override
+                    public long getDirectoryId()
+                    {
+                        return directoryId;
+                    }
+
+                    @Override
+                    public String getDisplayName()
+                    {
+                        return displayName;
+                    }
+
+                    @Override
+                    public String getEmailAddress()
+                    {
+                        return email;
+                    }
+
+                    @Override
+                    public String getName()
+                    {
+                        return name;
+                    }
+
+                    @Override
+                    public boolean isActive()
+                    {
+                        return active;
+                    }
+
+                    @Override
+                    public int compareTo(final User user)
+                    {
+                        return directoryUser.compareTo(user);
+                    }
+                };
+            }
+        };
     }
 }
