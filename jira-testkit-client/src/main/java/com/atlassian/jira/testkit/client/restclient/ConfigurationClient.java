ccea389605ba073d100cc4f3e5b42ae0ff69a644
Bartlomiej Lewandowski <blewandowski@atlassian.com>
2015-11-27 16:30:53 +0100

    ACJIRA-534: Added Response method for ConfigurationClient

--- jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/restclient/ConfigurationClient.java
+++ jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/restclient/ConfigurationClient.java
@@ -2,6 +2,7 @@ package com.atlassian.jira.testkit.client.restclient;
 
 import com.atlassian.jira.testkit.client.JIRAEnvironmentData;
 import com.atlassian.jira.testkit.client.RestApiClient;
+import com.sun.jersey.api.client.ClientResponse;
 
 public class ConfigurationClient extends RestApiClient<ConfigurationClient>
 {
@@ -13,5 +14,17 @@ public class ConfigurationClient extends RestApiClient<ConfigurationClient>
     public ConfigurationBean getConfiguration()
     {
         return createResource().path("configuration").get(ConfigurationBean.class);
+    }
+
+    public Response<ConfigurationBean> getConfigurationResponse()
+    {
+        return toResponse(new Method()
+        {
+            @Override
+            public ClientResponse call()
+            {
+                return createResource().path("configuration").get(ClientResponse.class);
+            }
+        }, ConfigurationBean.class);
     }
 }
