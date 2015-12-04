fb869a7a47a18d1b040334e4c37d7036d46c89ef
Dariusz Szuksztul <dszuksztul@atlassian.com>
2015-12-04 15:13:18 +0100

    JRA-33724 - transfer custom field usages in more consistent way

--- jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/restclient/VersionIssueCounts.java
+++ jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/restclient/VersionIssueCounts.java
@@ -22,54 +22,82 @@ import java.util.List;
  *
  * @since v4.4
  */
-public class VersionIssueCounts
-{
+public class VersionIssueCounts {
     public String self;
     public long issuesFixedCount;
     public long issuesAffectedCount;
     public long issueCountWithCustomFieldsShowingVersion;
-    public List<String> customFieldNames;
-    public List<Long> customFieldIds;
+    public List<VersionUsageInCustomFields> customFieldUsage;
 
-    public VersionIssueCounts self(URI self)
-    {
+    public VersionIssueCounts self(URI self) {
         this.self = self.toString();
         return this;
     }
 
-    public VersionIssueCounts self(String self)
-    {
+    public VersionIssueCounts self(String self) {
         this.self = self;
         return this;
     }
 
-    public VersionIssueCounts issuesFixedCount(long issuesFixedCount)
-    {
+    public VersionIssueCounts issuesFixedCount(long issuesFixedCount) {
         this.issuesFixedCount = issuesFixedCount;
         return this;
     }
 
-    public VersionIssueCounts issuesAffectedCount(long issuesAffectedCount)
-    {
+    public VersionIssueCounts issuesAffectedCount(long issuesAffectedCount) {
         this.issuesAffectedCount = issuesAffectedCount;
         return this;
     }
 
     @Override
-    public boolean equals(Object obj)
-    {
+    public boolean equals(Object obj) {
         return EqualsBuilder.reflectionEquals(this, obj);
     }
 
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    public static class VersionUsageInCustomFields {
+
+        public String fieldName;
+
+        public long customFieldId;
+
+        public long issueCountWithVersionInCustomField;
+
+        public VersionUsageInCustomFields() {
+        }
+
+        public VersionUsageInCustomFields(long customFieldId, String fieldName, long issueCountWithVersionInCustomField) {
+            this.fieldName = fieldName;
+            this.customFieldId = customFieldId;
+            this.issueCountWithVersionInCustomField = issueCountWithVersionInCustomField;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return EqualsBuilder.reflectionEquals(this, obj);
+        }
+
+        @Override
+        public int hashCode() {
+            return HashCodeBuilder.reflectionHashCode(this);
+        }
+
+        @Override
+        public String toString() {
+            return "VersionUsageInCustomFields{" +
+                    "fieldName='" + fieldName + '\'' +
+                    ", customFieldId=" + customFieldId +
+                    ", issueCountWithVersionInCustomField=" + issueCountWithVersionInCustomField +
+                    '}';
+        }
     }
 }
