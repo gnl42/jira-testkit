be8f9f9fe2a11515da1fe17f8a941341510a0a2e
Tomasz Kanafa <tkanafa@atlassian.com>
2017-10-30 14:22:09 +0100

    Revert "Merged in issue/MNSTR-1189-revert-rest-endpoint-priority-schemes (pull request #92)"
    This reverts commit e731a1fc1bc708cd015a8830095fdb4a5d922d06, reversing
    changes made to d8bd575393c390eb0afa1af192bd20d85fcc6925.

---
+++ jira-testkit-client/src/main/java/com/atlassian/jira/testkit/client/restclient/PrioritySchemeBeans.java
@@ -0,0 +1,11 @@
+package com.atlassian.jira.testkit.client.restclient;
+
+import java.util.List;
+
+public class PrioritySchemeBeans {
+    private List<PrioritySchemeBean> prioritySchemes;
+
+    public List<PrioritySchemeBean> getPrioritySchemes() {
+        return prioritySchemes;
+    }
+}
